ad3bca3ec5488fcfde1c23ab166ef403c83025be
Rukayyah Yussuf <ruky@Rukayyahs-Air.lan>
2026-04-14 16:37:31 +0100

    fix createClient syntax

--- app/api/update-rates/route.ts
+++ app/api/update-rates/route.ts
@@ -4,9 +4,11 @@ import { createClient } from "@supabase/supabase-js";
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
+);
+
 console.log("SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
 console.log("SERVICE KEY PRESENT:", !!process.env.SUPABASE_SERVICE_ROLE_KEY);
-);
+
 export async function GET() {
   try {
   const response = await fetch(
